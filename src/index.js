14ea381ea3ef1cc081f1df7b27fca21143073a09
Slava <slava.milin@gmail.com>
2020-11-17 01:44:58 +0300

    globby test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const globby = require("globby");
-const {resolve, posix} = require("path")
+const {resolve, join} = require("path")
 
 findFolders = async () => {
-  const pattern = posix.join(__dirname, '..', '*');
+  const pattern = join(__dirname, '..', '*');
   const folders = await globby([pattern, '!**/src'], {onlyDirectories: true, gitignore: true});
   console.log('Pattern: ', pattern);
   console.log('Результат: ', folders);
